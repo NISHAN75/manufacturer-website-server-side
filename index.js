fcd0464a03e13aeeb118f0b45a67507c301c76fc
nishandas <nishandas880@gmail.com>
2022-05-27 02:59:54 +0600

    remove user and order routhe set

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const app = express()
-const { MongoClient, ServerApiVersion ,ObjectId } = require('mongodb');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const { reset } = require('nodemon');
-require('dotenv').config();
+const express = require("express");
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { reset } = require("nodemon");
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
@@ -12,112 +12,148 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-function verifyJWT(req,res,next){
-  const authHeader=req.headers.authorization;
-  if(!authHeader){
-    return res.status(401).send({message: 'UnAuthorized access'})
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized access" });
   }
-  const token= authHeader.split(' ')[1];
+  const token = authHeader.split(" ")[1];
   // verify a token symmetric
-jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
-  if(err){
-    return res.status(403).send({message: 'Forbidden access'})
-  }
-  req.decoded=decoded
-  next();
-});
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
 }
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wfhxj.mongodb.net/?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
+async function run() {
+  try {
+    await client.connect();
+    const servicesCollection = client.db("bicycl_plus").collection("services");
+    const ordersCollection = client.db("bicycl_plus").collection("orders");
+    const usersCollection = client.db("bicycl_plus").collection("user");
 
-async function run(){
-  try{
-     await client.connect();
-     const servicesCollection = client.db('bicycl_plus').collection('services')
-     const ordersCollection = client.db('bicycl_plus').collection('orders')
-     const usersCollection = client.db('bicycl_plus').collection('user')
-    
-
-     app.get('/services', async(req,res) =>{
-      const query={};
+    app.get("/services", async (req, res) => {
+      const query = {};
       const cursor = servicesCollection.find(query);
-      const services= await cursor.toArray();
+      const services = await cursor.toArray();
       res.send(services);
-     });
-     app.get("/services/:id", async (req, res) => {
+    });
+    app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const part = await servicesCollection.findOne(query);
       res.send(part);
     });
     // put working
-    app.put('/user/:email', async(req,res) =>{
+    app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
-      const user=req.body
-      const filter={email:email};
+      const user = req.body;
+      const filter = { email: email };
       const options = { upsert: true };
       const updateDoc = {
-        $set:user,
+        $set: user,
       };
-      const result = await usersCollection.updateOne(filter, updateDoc, options);
-      const token=jwt.sign({email:email}, process.env.ACCESS_TOKEN_SECRET,{ expiresIn: '1h'} )
-      res.send({result, accessToken: token});
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1h" }
+      );
+      res.send({ result, accessToken: token });
+    });
+    app.get("/orders", verifyJWT, async (req, res) => {
+      const email = req.query.email;
+      const decodeEmail = req.decoded.email;
+      if (email === decodeEmail) {
+        const query = { userEmail: email };
+        const orders = await ordersCollection.find(query).toArray();
+        res.send(orders);
+      } else {
+        return res.status(403).send({ message: "Forbidden access" });
+      }
+    });
+    app.delete('/orders', async(req,res)=>{
+      const email = req.query.email;
+      console.log(email);
+      const filter= {userEmail:email};
+      const result = await ordersCollection.deleteOne(filter);
+      res.send(result)
+    });
+    app.get("/user", verifyJWT, async (req, res) => {
+      const users = await usersCollection.find().toArray();
+      res.send(users);
+    });
+    app.delete('/user/:email', verifyJWT, async(req,res)=>{
+      const email = req.params.email;
+      console.log(email);
+      const filter= {email:email};
+      const result = await usersCollection.deleteOne(filter);
+      res.send(result)
+    });
+    app.get('/admin/:email', async(req,res) =>{
+      const email = req.params.email;
+      const user = await usersCollection.findOne({email: email});
+      const isAdmin = user.role=== 'admin';
+      res.send({admin: isAdmin})
     })
-    app.get('/orders', verifyJWT, async(req,res) =>{
-      const email=req.query.email;
-      const decodeEmail=req.decoded.email
-      if(email === decodeEmail){
-        const query ={userEmail:email};
-      const orders=await ordersCollection.find(query).toArray();
-      res.send(orders);
+    // put working
+    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      console.log(requester);
+      const requesterAccount = await usersCollection.findOne({
+        email: requester,
+      });
+      console.log(requesterAccount);
+      if (requesterAccount.role === "admin") {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: "admin" },
+        };
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
       }
       else{
-        return res.status(403).send({message: 'Forbidden access'})
+        res.status(403).send({message: 'forbidden'})
       }
-      
     });
-    app.get('/user',verifyJWT, async(req,res)=>{
-      const users = await usersCollection.find().toArray();
-      res.send(users)
-    });
-    // put working
-    app.put('/user/admin/:email', async(req,res) =>{
-      const email = req.params.email;
-      const filter={email:email};
-      const updateDoc = {
-        $set:{role:'admin'},
-      };
-      const result = await usersCollection.updateOne(filter, updateDoc);
-      res.send(result);
-    })
-  
+
     // post working
-    app.post('/orders' , async(req,res) =>{
-      const orders= req.body;
-      const query = {partName:orders.partName,userEmail:orders.userEmail};
-      const exists= await ordersCollection.findOne(query)
-      if(exists){
-        return res.send({success: false , orders: exists})
+    app.post("/orders", async (req, res) => {
+      const orders = req.body;
+      const query = { partName: orders.partName, userEmail: orders.userEmail };
+      const exists = await ordersCollection.findOne(query);
+      if (exists) {
+        return res.send({ success: false, orders: exists });
       }
-    
       const result = await ordersCollection.insertOne(orders);
-      res.send({success: true , result});
+      res.send({ success: true, result });
     });
 
     
+  } finally {
   }
-  finally{}
 }
 run();
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+  console.log(`Example app listening on port ${port}`);
+});
